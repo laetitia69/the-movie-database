ae2e72a7eac50729ab5d6b3c8d926c102c2b76e4
laetitia69 <titi-fernandes@hotmail.fr>
2018-08-01 17:00:01 +0200

    commit

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,13 +1,5 @@
 class MovieController < ApplicationController
   def index
-  	@movies = Movie.new
-  end
-
-  def create
-  	movie = Movie.create(params.require(:movie).permit(:title, :release_year, :director))
-  end
-
-  def show
   	@movies = Movie.all
-  end
+  end 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+	
+def list_movies
+	
+	Director.destroy_all
+	@directors = Director.create([{first_name: "James", last_name: "DeMonaco"},
+								  {first_name: "Corin", last_name: "Hardy"},
+								  {first_name: "Jeff", last_name: "Wadlow"},
+								  {first_name: "John", last_name: "Krasinski"}])
 
-	Movie.all.destroy
-	movie = Movie.create(title: "American nightmare 1", release_year: 2013, director: "James DeMonaco")
+	d = Director.all
+
+	d.each do |dir|
+		
+		Movie.destroy_all
+		@movies = Movie.create([{title: "American nightmare 1", release_year: 2013, director_id: "#{dir.id}"},
+								{title: "La nonne", release_year: 2018, director_id: "#{dir.id}"},
+								{title: "Action ou vérité", release_year: 2018, director_id: "#{dir.id}"},
+								{title: "Sans un bruit", release_year: 2018, director_id: "#{dir.id}"}])
+	end
+
+end
+puts list_movies.inspect
